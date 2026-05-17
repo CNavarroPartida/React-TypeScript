b4a98e8fa8a4f9000dad228b52efa372e582a042
Carlos Navarro <carlos_a._93@hotmail.com>
2026-05-16 21:47:52 -0600

    Proyecto 01 - Carrito de compras React + JavaScript

---
+++ guitarla/src/App.jsx
@@ -0,0 +1,98 @@
+import {useState, useEffect} from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Guitar from "./components/Guitar";
+import {db} from "./data/db";
+
+function App() {
+
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem("cart");
+        return localStorageCart ? JSON.parse(localStorageCart) : [];
+    }
+
+    const [data] = useState(db);
+    const [cart, setCart] = useState(initialCart);
+    const MAX_ITEMS = 5;
+    const MIN_ITEMS = 1;
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart])
+
+    function addToCart(item) {
+        const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+        if (itemExist >= 0) {
+            const updateCart = [...cart];
+            updateCart[itemExist].quantity++;
+            setCart(updateCart);
+        } else {
+            item.quantity = 1;
+            setCart([...cart, item]);
+        }
+
+
+    }
+
+    function removeFromCart(id) {
+        setCart(prevCart => prevCart.filter((guitar) => guitar.id !== id));
+    }
+
+    function increaseQuantity(id) {
+        const updateCart = cart.map((item) => {
+            if(item.id === id && item.quantity < MAX_ITEMS) {
+                return {
+                    ...item,
+                    quantity: item.quantity + 1};
+            }
+            return item;
+        });
+
+        setCart(updateCart);
+    }
+
+    function decreaseQuantity(id) {
+        const updateCart = cart.map((item) => {
+            if(item.id === id && item.quantity > MIN_ITEMS) {
+                return {
+                    ...item,
+                    quantity: item.quantity - 1};
+            }
+            return item;
+        });
+
+        setCart(updateCart);
+    }
+
+    function clearCart() {
+        setCart([]);
+    }
+
+    return (
+        <>
+            <Header
+                cart={cart}
+                removeFromCart={removeFromCart}
+                increaseQuantity={increaseQuantity}
+                decreaseQuantity={decreaseQuantity}
+                clearCart={clearCart}
+            />
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">
+                    {data?.map((guitar) => (
+                        <Guitar
+                            key={guitar.id}
+                            guitar={guitar}
+                            addToCart={addToCart}
+                        />
+                    ))}
+                </div>
+            </main>
+            <Footer/>
+        </>
+    )
+}
+
+export default App
