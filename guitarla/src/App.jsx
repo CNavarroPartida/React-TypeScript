2a8d3d3cae05b87c90c8b41375fa784088266b93
Carlos Navarro <carlos_a._93@hotmail.com>
2026-05-17 19:09:46 -0600

    Proyecto 01 - Creando customs hooks personalizados

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -1,72 +1,12 @@
-import {useState, useEffect} from "react";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Guitar from "./components/Guitar";
-import {db} from "./data/db";
+import {useCart} from "./hooks/useCart";
 
 function App() {
 
-    const initialCart = () => {
-        const localStorageCart = localStorage.getItem("cart");
-        return localStorageCart ? JSON.parse(localStorageCart) : [];
-    }
-
-    const [data] = useState(db);
-    const [cart, setCart] = useState(initialCart);
-    const MAX_ITEMS = 5;
-    const MIN_ITEMS = 1;
-
-    useEffect(() => {
-        localStorage.setItem("cart", JSON.stringify(cart));
-    }, [cart])
-
-    function addToCart(item) {
-        const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
-        if (itemExist >= 0) {
-            const updateCart = [...cart];
-            updateCart[itemExist].quantity++;
-            setCart(updateCart);
-        } else {
-            item.quantity = 1;
-            setCart([...cart, item]);
-        }
-
-
-    }
-
-    function removeFromCart(id) {
-        setCart(prevCart => prevCart.filter((guitar) => guitar.id !== id));
-    }
-
-    function increaseQuantity(id) {
-        const updateCart = cart.map((item) => {
-            if(item.id === id && item.quantity < MAX_ITEMS) {
-                return {
-                    ...item,
-                    quantity: item.quantity + 1};
-            }
-            return item;
-        });
-
-        setCart(updateCart);
-    }
-
-    function decreaseQuantity(id) {
-        const updateCart = cart.map((item) => {
-            if(item.id === id && item.quantity > MIN_ITEMS) {
-                return {
-                    ...item,
-                    quantity: item.quantity - 1};
-            }
-            return item;
-        });
-
-        setCart(updateCart);
-    }
-
-    function clearCart() {
-        setCart([]);
-    }
+    const {data, cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal} = useCart();
 
     return (
         <>
@@ -76,6 +16,8 @@ function App() {
                 increaseQuantity={increaseQuantity}
                 decreaseQuantity={decreaseQuantity}
                 clearCart={clearCart}
+                isEmpty={isEmpty}
+                cartTotal={cartTotal}
             />
             <main className="container-xl mt-5">
                 <h2 className="text-center">Nuestra Colección</h2>
